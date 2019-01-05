16ce379b627ac7bda42a75fa50102639639c1c72
thk2b <theokobb@gmail.com>
2019-01-05 10:14:45 -0800

    [update] cast port, more accurate info message

--- src/client/init.c
+++ src/client/init.c
@@ -12,13 +12,13 @@ int			init(int *cconp, t_opts *opts)
 	int					sock;
 	struct sockaddr_in	addr;
 
-	info("attempting connection to %s:%d", opts->ip, opts->port);
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		return (error(errno, "socket"));
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(opts->port);
+	addr.sin_port = htons((uint32_t)opts->port);
 	if (inet_aton(opts->ip, &addr.sin_addr) != 1)
 		return (error(errno, "inet_aton"));
+	info("attempting connection to %s:%d", inet_ntoa(addr.sin_addr), opts->port);
 	if (connect(sock, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) < 0)
 		return (error(errno, "connect"));
 	info("connected to %s:%d", opts->ip, opts->port);
