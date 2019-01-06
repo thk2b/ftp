733752c8879caeb07323b3c5faca9a643e54d551
thk2b <theokobb@gmail.com>
2019-01-06 07:36:20 -0800

    [update] merge protocol and handlers tables. rename protocol entry to request, request to request_ctx

--- src/protocol.c
+++ src/protocol.c
@@ -2,31 +2,20 @@
 #include			<libft.h>
 #include			<string.h>
 
-t_protocol_entry	g_protocol[] = {
-	{ "QUIT"	, {0, 0}	},
-	{ "LIST"	, {0, 1}	},
-	{ "CWD"		, {0, 1}	},
-	{ "PWD"		, {0, 0}	},
-	{ "RETR"	, {1, 2}	},
-	{ "STOR"	, {1, 2}	},
-	{ "QUIT"	, {0, 0}	},
-	{ NULL		, {0, 0}	}
+t_request	g_protocol[] = {
+	{ "QUIT"	, {0, 0}	, quit_handler	},
+	{ "LIST"	, {0, 1}	, list_handler	},
+	{ "CWD"		, {0, 1}	, cwd_handler	},
+	{ "PWD"		, {0, 0}	, pwd_handler	},
+	{ "RETR"	, {1, 2}	, retr_handler	},
+	{ "STOR"	, {1, 2}	, stor_handler	},
+	{ "QUIT"	, {0, 0}	, quit_handler	},
+	{ NULL		, {0, 0}	, NULL			}
 };
 
-t_cmd_handler		g_handlers[] = {
-	{ "QUIT"	, quit_handler	},
-	{ "LIST"	, list_handler	},
-	{ "CWD"		, cwd_handler	},
-	{ "PWD"		, pwd_handler	},
-	{ "RETR"	, retr_handler	},
-	{ "STOR"	, stor_handler	},
-	{ "QUIT"	, quit_handler	},
-	{ NULL		, NULL			}
-};
-
-t_cmd_handler_fn	find_handler(t_request *req)
+t_request_handler_fn	find_handler(t_request_ctx *req)
 {
-	return (g_handlers[req->code].fn);
+	return (g_protocol[req->code].fn);
 }
 
 int					validate_arguments(size_t i, char **cmd)
--- src/protocol.h
+++ src/protocol.h
@@ -6,40 +6,41 @@
 /*
 **	verb and argument requirement of a request
 */
-typedef struct	s_protocol_entry
+typedef struct	s_request
 {
-	char		*name;
-	size_t		arg_num[2];
-}				t_protocol_entry;
+	char					*name;
+	size_t					arg_num[2];
+	t_request_handler_fn	fn;
+}				t_request;
 
 int				validate_arguments(size_t i, char **cmd);
 
 /*
 **	instance of a protocol entry
 */
-typedef struct	s_request
+typedef struct	s_request_ctx
 {
 	char		**args;
 	size_t		code;
-}				t_request;
+}				t_request_ctx;
 
 /*
 **	request handlers
 */
-typedef int			(*t_cmd_handler_fn)(int ccon, int *dcon, t_request *req);
+typedef int			(*t_request_handler_fn)(int ccon, int *dcon, t_request *req);
 typedef struct		s_cmd_handler
 {
 	char				*name;
-	t_cmd_handler_fn	fn;
+	t_request_handler_fn	fn;
 }					t_cmd_handler;
 
-t_cmd_handler_fn	find_handler(t_request *req);
-int					quit_handler(int ccon, int *dcon, t_request *req);
-int					list_handler(int ccon, int *dcon, t_request *req);
-int					cwd_handler(int ccon, int *dcon, t_request *req);
-int					pwd_handler(int ccon, int *dcon, t_request *req);
-int					retr_handler(int ccon, int *dcon, t_request *req);
-int					stor_handler(int ccon, int *dcon, t_request *req);
-int					quit_handler(int ccon, int *dcon, t_request *req);
+t_request_handler_fn	find_handler(t_request_ctx *req);
+int						quit_handler(int ccon, int *dcon, t_request *req);
+int						list_handler(int ccon, int *dcon, t_request *req);
+int						cwd_handler(int ccon, int *dcon, t_request *req);
+int						pwd_handler(int ccon, int *dcon, t_request *req);
+int						retr_handler(int ccon, int *dcon, t_request *req);
+int						stor_handler(int ccon, int *dcon, t_request *req);
+int						quit_handler(int ccon, int *dcon, t_request *req);
 
 #endif
