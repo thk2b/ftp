db170f26ce5a76b833c799f5170ec1795c098aff
thk2b <theokobb@gmail.com>
2019-01-07 09:03:56 -0800

    [implement] prepare server for setting up data connection

--- src/client/handlers/quit_handler.c
+++ src/client/handlers/quit_handler.c
@@ -8,7 +8,7 @@ int				quit_handler(int ccon, int *dcon, t_request_ctx *req)
 	(void)dcon;
 	if ((status = send_request(ccon, req)))
 		return (1);
-	if (status == 0 && (status = get_response(ccon)) == 221)
+	if (status == 0 && (status = get_response(ccon, NULL)) == 221)
 		return (-1);
 	return (status);
 }
--- src/server/handlers/pasv_handler.c
+++ src/server/handlers/pasv_handler.c
@@ -5,5 +5,6 @@ int			pasv_handler(int ccon, int *dcon, t_request_ctx *req)
 	(void)ccon;
 	(void)dcon;
 	(void)req;
+	info ("attempting to open data connection");
 	return (1);
 }
