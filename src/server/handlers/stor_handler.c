a1fdc4be7289c647a966977ec6c7ecd0b27132d0
thk2b <theokobb@gmail.com>
2019-01-08 10:25:47 -0800

    [update] improve error handling

--- src/server/handlers/stor_handler.c
+++ src/server/handlers/stor_handler.c
@@ -5,13 +5,30 @@
 #include		<unistd.h>
 #include		<errno.h>
 
+static int		read_file(int from, int to)
+{
+	char		buf[BUF_SIZE];
+	ssize_t		num_read;
+
+	while ((num_read = read(from, buf, BUF_SIZE)) > 0)
+	{
+		if (write(to, buf, num_read) != num_read)
+			return (error(451, "write"));
+		if (num_read != BUF_SIZE)
+			break ;
+	}
+	if (num_read == -1)
+		return (error(451, "read"));
+	return (0);
+}
+
 //TODO: send 451 instead of 550
 static int		do_stor(int ccon, int *dcon, t_request_ctx *req)
 {
 	int			fd;
-	char		buf[BUF_SIZE];
-	ssize_t		num_read;
+	int			status;
 
+	status = 0;
 	if (*dcon == -1)
 	{
 		if (send_response(150, ccon) || pasv_handler(ccon, dcon, NULL))
@@ -20,18 +37,12 @@ static int		do_stor(int ccon, int *dcon, t_request_ctx *req)
 	else if (send_response(125, ccon))
 		return (1);
 	if ((fd = open(req->args[req->args[2] ? 2 : 1], O_WRONLY | O_CREAT, 0775)) == -1)
-		return (error(550, "open"));
+		status = error(550, "open");
 	info("reading data connection, creating file");
-	while ((num_read = read(*dcon, buf, BUF_SIZE)) > 0)
-	{
-		if (write(fd, buf, num_read) != num_read)
-			return (error(550, "write"));
-		if (num_read != BUF_SIZE)
-			break ;
-	}
-	if (num_read == -1)
-		return (error(550, "read"));
-	return (0);
+	if (status)
+		status = read_file(*dcon, fd);
+	close(fd);
+	return (status);
 }
 
 int				stor_handler(int ccon, int *dcon, t_request_ctx *req)
