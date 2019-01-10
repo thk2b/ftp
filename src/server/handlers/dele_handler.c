1a097aaf7bbf4f95955896a5a60d9558be705c88
thk2b <theokobb@gmail.com>
2019-01-10 15:34:47 -0800

    [update] handle absolute path in rm

--- src/server/handlers/dele_handler.c
+++ src/server/handlers/dele_handler.c
@@ -3,25 +3,43 @@
 #include	<errno.h>
 #include	<unistd.h>
 #include	<string.h>
+#include	<libft.h>
+#include	<limits.h>
+
+static int	secure_absolute_path(char *filename, char **dst)
+{
+	char		real_pwd[PATH_MAX];
+
+	if (getcwd(real_pwd, PATH_MAX) == NULL)
+		return (1);
+	if ((*dst = ft_strjoin(real_pwd, filename)) == NULL)
+		return (1);
+	return (0);
+}
 
 int			dele_handler(int ccon, int *dcon, t_request_ctx *req, void *ctx)
 {
 	char		*tmp;
+	int			status;
 	t_client	*c;
 
 	c = (t_client*)ctx;
 	(void)dcon;
-
-	if (strncmp(req->args[1], "/", 1) == 0 || strcmp(req->args[1], ".") == 0 || strncmp(req->args[1], "..", 2) == 0)
+	if ((status = path_join(&tmp, c->pwd, req->args[1])) == 1)
+		return (error_conn(ccon, 451, 1, "path_join"));
+	else if (status == -1)
 		return (send_response(553, ccon));
-	if (path_join(&tmp, c->pwd, req->args[1]))
-		return (send_response(553, ccon));
-	free(tmp);
-	if (unlink(req->args[1]))
+	if (secure_absolute_path(tmp, &tmp))
+		return (error_conn(ccon, 451, 1, "secure absolute path"));
+	free(req->args[1]);
+	req->args[1] = tmp;
+	info("unlink %s", tmp);
+	status = unlink(tmp);
+	if (status)
 	{
 		if (errno == ENOENT || errno == EPERM)
 			return (send_response(553, ccon));
-		return (send_response(451, ccon));
+		return (error_conn(ccon, 451, 1, "unlink"));
 	}
 	return (send_response(250, ccon));
 }
